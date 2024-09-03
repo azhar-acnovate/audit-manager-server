140aa1cea39127b7ed3ff1434d37fd33f7561409
Azhardeen <azharudeen.nazeerdeen@acnovate.com>
2024-09-03 10:00:28 +0530

    add object and attribute changes domains

---
+++ src/main/java/com/acnovate/audit_manager/domain/AuditAttributeChangeTracker.java
@@ -0,0 +1,73 @@
+package com.acnovate.audit_manager.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "audit_attribute_change_tracker")
+public class AuditAttributeChangeTracker extends AuditEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String attributeName;
+	private String oldValue;
+	private String newValue;
+	private String changedBy;
+
+	@ManyToOne
+	@JoinColumn(name = "audit_object_change_tracker_id")
+	private AuditObjectChangeTracker auditObjectChangeTracker;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAttributeName() {
+		return attributeName;
+	}
+
+	public void setAttributeName(String attributeName) {
+		this.attributeName = attributeName;
+	}
+
+	public String getOldValue() {
+		return oldValue;
+	}
+
+	public void setOldValue(String oldValue) {
+		this.oldValue = oldValue;
+	}
+
+	public String getNewValue() {
+		return newValue;
+	}
+
+	public void setNewValue(String newValue) {
+		this.newValue = newValue;
+	}
+
+	public String getChangedBy() {
+		return changedBy;
+	}
+
+	public void setChangedBy(String changedBy) {
+		this.changedBy = changedBy;
+	}
+
+	public AuditObjectChangeTracker getAuditObjectChangeTracker() {
+		return auditObjectChangeTracker;
+	}
+
+	public void setAuditObjectChangeTracker(AuditObjectChangeTracker auditObjectChangeTracker) {
+		this.auditObjectChangeTracker = auditObjectChangeTracker;
+	}
+}
---
+++ src/main/java/com/acnovate/audit_manager/domain/enums/AuditLogEntityTypeEnum.java
@@ -0,0 +1,5 @@
+package com.acnovate.audit_manager.domain.enums;
+
+public enum AuditLogEntityTypeEnum {
+	PRODUCT, SEASON, COLORWAY, OTHERS
+}
